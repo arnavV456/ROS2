5ef60222d4de4654c9ba2d97c57a20ee57c89488
Arnav Verma <147163265+arnavV456@users.noreply.github.com>
2026-06-30 17:45:47 +0530

    Add files via upload

---
+++ python/smartphone.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from example_interfaces.msg import String
+
+
+class SmartphoneNode(Node): # MODIFY NAME
+    def __init__(self):
+        super().__init__("smartphone") # MODIFY NAME
+        self.subscriber_ = self.create_subscription(String,"robot_news", self.robot_news_callback, 10)
+    def robot_news_callback(self, msg):
+        self.get_logger().info(f"Received : {msg.data}")  # Log the received news
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = SmartphoneNode() # MODIFY NAME
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
